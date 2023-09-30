018712e941cee71bd710183e7b65c46193be8425
qasemB <mahdicmptr@gmail.com>
2023-09-30 07:36:51 +0330

    Add project files.

---
+++ fileSeek/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace fileSeek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(460, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(372, 21);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "متن مورد نظر";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "جستجو";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(332, 71);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(94, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "مسیر فولدر فایل ها";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(10, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(460, 22);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(404, 181);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "نام فایل";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 212);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(457, 155);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 143);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(428, 23);
+            this.progressBar1.TabIndex = 9;
+            this.progressBar1.Tag = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(447, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 385);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "جستجو در متن - حاج مهدی";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ fileSeek/Form1.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Range = Microsoft.Office.Interop.Excel.Range;
+
+namespace fileSeek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            richTextBox1.Text = "";
+            string directoryPath = textBox2.Text;
+            string[] docxFiles = Directory.GetFiles(directoryPath, "*");
+
+
+
+            var text = textBox1.Text;
+
+            var i = 0;
+
+            foreach (string file in docxFiles)
+            {
+                i++;
+                if (!file.Contains("~") || !file.Contains("$"))
+                {
+                    
+
+                    if (file.Contains(".xlsx"))
+                    {
+
+                        Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+
+                        // Open the Excel file
+                        Workbook workbook = excelApp.Workbooks.Open(file);
+
+                        // Get the first worksheet in the workbook
+                        Worksheet worksheet = workbook.Sheets[1];
+
+                        // Get the used range of the worksheet
+                        Range usedRange = worksheet.UsedRange;
+
+                        // Loop through each cell in the used range
+                        foreach (Range cell in usedRange.Cells)
+                        {
+                            // Check if the cell value contains the search keyword
+                            if (cell.Value != null && cell.Value.ToString().Contains(text))
+                            {
+                                richTextBox1.Text = richTextBox1.Text + "\n" + file + " - " + cell.Address;
+
+                            }
+                        }
+
+                        // Close the workbook and release resources
+                        workbook.Close();
+                        excelApp.Quit();
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                    }
+                    else if (file.Contains(".docx"))
+                    {            // Create an instance of the Word application
+                        Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();    // Open the Word document
+                        Document wordDoc = wordApp.Documents.Open(file);
+
+                        // Read the content of the document
+                        string content = wordDoc.Content.Text;
+
+                        if (content.Contains(text))
+                        {
+                            //listView1.Items.Add(file);
+                            richTextBox1.Text += "\n" + file;
+                        }
+                        wordDoc.Close();
+                    }
+                    else
+                    {
+                        richTextBox1.Text += "\nفرمت فایل پشتیبانی نمی شود " + file + "\n";
+                    }
+
+                    
+                }
+
+                progressBar1.Value += (int)Math.Round((decimal)((100 / docxFiles.Length)));
+
+                if (i == docxFiles.Length) progressBar1.Value = 100;
+
+                label4.Text = progressBar1.Value.ToString() + "%";
+
+            }
+
+            richTextBox1.Text = richTextBox1.Text + "\n" + "پایان";
+
+
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            //folderBrowserDialog.SelectedPath = @"C:\";
+            folderBrowserDialog.Description = "Select a folder";
+            folderBrowserDialog.ShowNewFolderButton = true;
+            folderBrowserDialog.ShowDialog();
+            textBox2.Text = folderBrowserDialog.SelectedPath;
+
+
+        }
+
+        
+    }
+
+
+
+}
